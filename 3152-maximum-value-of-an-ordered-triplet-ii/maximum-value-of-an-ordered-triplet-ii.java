58d2e3f9ed0062fc5f40acbfda78ba7ee1a163ac
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2024-09-18 20:03:44 +0530

    Time: 2 ms (97.04%) | Memory: 57.4 MB (76.30%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.java
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.java
@@ -1,23 +1,32 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-      long m=0;
-      int[] l=new int[nums.length];
-      int[] r=new int[nums.length];
-      l[0]=nums[0];
-      r[nums.length-1]=nums[nums.length-1];
-      for(int i=1;i<nums.length;i++)
-      {
-        l[i]=Math.max(l[i-1],nums[i]);
-      }
-      for(int i=nums.length-2;i>=0;i--)
-      {
-        r[i]=Math.max(r[i+1],nums[i]);
-      }
-      for(int i=1;i<nums.length-1;i++)
-      {
-         long s=(long)(l[i-1]-nums[i])*r[i+1];
-         m=Math.max(s,m);
-      }
-      return m;
+    //   long m=0;
+    //   int[] l=new int[nums.length];
+    //   int[] r=new int[nums.length];
+    //   l[0]=nums[0];
+    //   r[nums.length-1]=nums[nums.length-1];
+    //   for(int i=1;i<nums.length;i++)
+    //   {
+    //     l[i]=Math.max(l[i-1],nums[i]);
+    //   }
+    //   for(int i=nums.length-2;i>=0;i--)
+    //   {
+    //     r[i]=Math.max(r[i+1],nums[i]);
+    //   }
+    //   for(int i=1;i<nums.length-1;i++)
+    //   {
+    //      long s=(long)(l[i-1]-nums[i])*r[i+1];
+    //      m=Math.max(s,m);
+    //   }
+    //   return m;
+
+    long mul=0,diff=0,max=0;
+    for(int x:nums)
+    {
+      mul=Math.max(mul,x*diff);
+       max=Math.max(max,x);
+       diff=Math.max(diff,max-x);
+    }
+    return mul;
     }
 }
