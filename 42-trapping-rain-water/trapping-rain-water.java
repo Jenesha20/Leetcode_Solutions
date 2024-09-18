302f416e7b5e245ff50bca38729ca7a1a47bb7ff
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2024-09-18 20:35:08 +0530

    Time: 1 ms (67.35%) | Memory: 46.1 MB (64.37%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,23 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-      int[] l=new int[height.length];
-      int[] r=new int[height.length];
-      int t=0;
-      l[0]=height[0];
-      r[height.length-1]=height[height.length-1];
-      for(int i=1;i<height.length;i++)
-      {
-        l[i]=Math.max(l[i-1],height[i]);
-      }
-      for(int i=height.length-2;i>=0;i--)
-      {
-        r[i]=Math.max(r[i+1],height[i]);
-      }
-      for(int i=1;i<height.length-1;i++)
-      {
-        t+=Math.min(l[i],r[i])-height[i];
-      }
-      return t;
+    //   int[] l=new int[height.length];
+    //   int[] r=new int[height.length];
+    //   int t=0;
+    //   l[0]=height[0];
+    //   r[height.length-1]=height[height.length-1];
+    //   for(int i=1;i<height.length;i++)
+    //   {
+    //     l[i]=Math.max(l[i-1],height[i]);
+    //   }
+    //   for(int i=height.length-2;i>=0;i--)
+    //   {
+    //     r[i]=Math.max(r[i+1],height[i]);
+    //   }
+    //   for(int i=1;i<height.length-1;i++)
+    //   {
+    //     t+=Math.min(l[i],r[i])-height[i];
+    //   }
+    //   return t;
     // int l=0,r=height.length-1,w=0;
     // int lm=height[l],rm=height[r];
     // while(l<r)
@@ -36,5 +36,23 @@ class Solution {
     //     }
     // }
     // return w;
+    int[] l=new int[height.length];
+    int[] r=new int[height.length];
+    int t=0;
+    l[0]=height[0];
+    r[height.length-1]=height[height.length-1];
+    for(int i=1;i<height.length;i++)
+    {
+        l[i]=Math.max(l[i-1],height[i]);
+    }
+    for(int i=height.length-2;i>=0;i--)
+    {
+        r[i]=Math.max(r[i+1],height[i]);
+    }
+    for(int i=1;i<height.length-1;i++)
+    {
+        t+=Math.min(l[i],r[i])-height[i];
+    }
+    return t;
     }
 }
