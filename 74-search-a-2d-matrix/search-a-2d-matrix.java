6aade667750622477847c4e0a41f1b8ee5945084
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2024-09-24 11:02:48 +0530

    Time: 157 ms (100.00%) | Memory: 42.6 MB (6.42%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,28 +1,28 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-    //   List<Integer> l=new ArrayList<>();
-    //   for(int i=0;i<matrix.length;i++)
-    //   {
-    //     for(int j=0;j<matrix[0].length;j++)
-    //     {
-    //         l.add(matrix[i][j]);
-    //     }
-    //   } 
-    //   for(int x:l)
-    //   if(l.contains(target))
-    //   return true;
-    //   return false;
-    int l=0;
-    int  r=matrix[0].length-1;
-    while(l<matrix.length && r>=0)
-    {
-        if(matrix[l][r]==target)
-        return true;
-        if(matrix[l][r]<target)
-        l++;
-        else
-        r--;
-    }
-    return false;
+      List<Integer> l=new ArrayList<>();
+      for(int i=0;i<matrix.length;i++)
+      {
+        for(int j=0;j<matrix[0].length;j++)
+        {
+            l.add(matrix[i][j]);
+        }
+      } 
+      for(int x:l)
+      if(l.contains(target))
+      return true;
+      return false;
+    // int l=0;
+    // int  r=matrix[0].length-1;
+    // while(l<matrix.length && r>=0)
+    // {
+    //     if(matrix[l][r]==target)
+    //     return true;
+    //     if(matrix[l][r]<target)
+    //     l++;
+    //     else
+    //     r--;
+    // }
+    // return false;
     }
 }
