d97e483460aca8314780aec374cc39e24840c207
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2024-10-19 15:53:11 +0530

    Time: 1 ms (81.63%) | Memory: 41.2 MB (66.48%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         HashMap<Character,String> hmap=new HashMap<>();
-        String[] str=s.split(" ");
-         if(pattern.length()!=str.length)
+        String[] ns=s.split(" ");
+        if(pattern.length()!=ns.length)
         return false;
-        for(int i=0;i<str.length;i++)
+        for(int i=0;i<ns.length;i++)
         {
-            if(!hmap.containsKey(pattern.charAt(i)) && !hmap.containsValue(str[i]))
-            hmap.put(pattern.charAt(i),str[i]);
-            else if(hmap.containsKey(pattern.charAt(i)))
+            char c=pattern.charAt(i);
+            if(!hmap.containsKey(c))
             {
-                if(!hmap.get(pattern.charAt(i)).equals(str[i]))
-                return false;
+                if(hmap.containsValue(ns[i]))
+                {
+                    return false;
+                }
+                hmap.put(c,ns[i]);
             }
-            else if(hmap.containsValue(str[i]))
+            else if(!(hmap.get(c).equals(ns[i])))
             {
-                if(!hmap.containsKey(pattern.charAt(i)))
-                return false;
+               return false;
             }
         }
         return true;
