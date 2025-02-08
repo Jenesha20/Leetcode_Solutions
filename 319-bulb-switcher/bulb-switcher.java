1c67b81b5c7f356278afcb606616f76972b50212
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2025-02-08 14:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (15.92%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.java
+++ 319-bulb-switcher/bulb-switcher.java
@@ -1,29 +1,30 @@
 class Solution {
     public int bulbSwitch(int n) {
-      if(n==0)
-      return 0;
-      if(n==1)
-      return 1;
-      if(n==99999999)
-      return 9999;
-      if(n==100000000)
-      return 10000;
-      boolean[] a=new boolean[n];
-      int c=0;
-      Arrays.fill(a,true);
-      for(int i=2;i<n;i++)
-      {
-        for(int j=i-1;j<n;j+=i)
-        {
-            a[j]=!(a[j]);
-        }
-      }
-      a[n-1]=!(a[n-1]);
-      for(int i=0;i<n;i++)
-      {
-        if(a[i])
-        c++;
-      }
-      return c;
+    //   if(n==0)
+    //   return 0;
+    //   if(n==1)
+    //   return 1;
+    //   if(n==99999999)
+    //   return 9999;
+    //   if(n==100000000)
+    //   return 10000;
+    //   boolean[] a=new boolean[n];
+    //   int c=0;
+    //   Arrays.fill(a,true);
+    //   for(int i=2;i<n;i++)
+    //   {
+    //     for(int j=i-1;j<n;j+=i)
+    //     {
+    //         a[j]=!(a[j]);
+    //     }
+    //   }
+    //   a[n-1]=!(a[n-1]);
+    //   for(int i=0;i<n;i++)
+    //   {
+    //     if(a[i])
+    //     c++;
+    //   }
+    //   return c;
+    return (int) Math.sqrt(n);
     }
 }
