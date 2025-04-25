7881cc3caf2226f6f850226dc0451da5a92ee75c
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2025-04-25 12:02:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (16.79%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -1,33 +1,47 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode oddEvenList(ListNode head) {
+//         List<Integer> l=new ArrayList<>();
+//         ListNode temp=head;
+//         while(temp!=null)
+//         {
+//             l.add(temp.val);
+//             temp=temp.next;
+//         }
+//         temp=head;
+//         for(int i=0;i<l.size();i+=2)
+//         {
+//             temp.val=l.get(i);
+//             temp=temp.next;
+//         }
+//         for(int i=1;i<l.size();i+=2)
+//         {
+//             temp.val=l.get(i);
+//             temp=temp.next;
+//         }
+//         return head;
+//     }
+// }
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        List<Integer> l=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null)
-        {
-            l.add(temp.val);
-            temp=temp.next;
+        if(head == null || head.next == null) return head;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while(even!=null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
         }
-        temp=head;
-        for(int i=0;i<l.size();i+=2)
-        {
-            temp.val=l.get(i);
-            temp=temp.next;
-        }
-        for(int i=1;i<l.size();i+=2)
-        {
-            temp.val=l.get(i);
-            temp=temp.next;
-        }
+        odd.next = evenHead;
         return head;
     }
 }
