8d0e09c933857c253ae30012e793ffe3244bd81d
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2025-04-29 10:13:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (49.37%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -18,20 +18,35 @@ class Solution {
 
         //memozation
 
-         int[] dp=new int[n+1];
-         Arrays.fill(dp,-1);
-         return helper(n,dp);
+        //  int[] dp=new int[n+1];
+        //  Arrays.fill(dp,-1);
+        //  return helper(n,dp);
+
+
+        //tabulation
+
+        int[] dp=new int[n+1];
+        if(n<=1)
+        return n;
+	    dp[0]=0;
+	    dp[1]=1;
+	    for(int i=2;i<=n;i++)
+	    {
+	        dp[i]=dp[i-1]+dp[i-2];
+	    }
+	    return dp[n];
 
 
     
     }
-    public static int helper(int n,int[] dp)
-    {
-        if(n<=1)
-         return n;
-         if(dp[n]!=-1)
-         return dp[n];
-         dp[n]=helper(n-2,dp)+helper(n-1,dp);
-         return dp[n];
-    }
+    //memoization
+    // public static int helper(int n,int[] dp)
+    // {
+    //     if(n<=1)
+    //      return n;
+    //      if(dp[n]!=-1)
+    //      return dp[n];
+    //      dp[n]=helper(n-2,dp)+helper(n-1,dp);
+    //      return dp[n];
+    // }
 }
