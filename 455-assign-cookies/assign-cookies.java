c080528fa7c5fce4276662fab72c30323abee478
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2025-04-30 11:56:32 +0530

    Time: 10 ms (80.64%) | Memory: 48.9 MB (16.04%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -4,17 +4,29 @@ class Solution {
         int c=0;
         Arrays.sort(g);
         Arrays.sort(s);
-        for(int i=0;i<s.length;i++)
+        // for(int i=0;i<s.length;i++)
+        // {
+        //     for(int j=0;j<g.length;j++)
+        //     {
+        //         if(s[i]>=g[j] && g[j]!=-1 && s[i]!=-1)
+        //         {
+        //             g[j]=-1;
+        //             s[i]=-1;
+        //             c++;
+        //         }
+        //     }
+        // }
+        // return c;
+
+        int ch=0,co=0;
+        while(ch<g.length && co< s.length)
         {
-            for(int j=0;j<g.length;j++)
+            if(s[co]>=g[ch])
             {
-                if(s[i]>=g[j] && g[j]!=-1 && s[i]!=-1)
-                {
-                    g[j]=-1;
-                    s[i]=-1;
-                    c++;
-                }
+                c++;
+                ch++;
             }
+            co++;
         }
         return c;
     }
