cce8a45827a8c0ac0aff48c04bf3d3ed01f006d5
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2025-04-30 10:21:44 +0530

    Time: 1257 ms (5.01%) | Memory: 46.1 MB (21.66%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,41 @@
+import java.util.*;
 class Solution {
     public boolean canJump(int[] nums) {
-     int m=0;
-     for(int i=0;i<nums.length;i++)
-     {
-        if(i>m)
+    //     int max=0;
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         if(i>max)
+    //         {
+    //             return false;
+    //         }
+    //         max=Math.max(max,i+nums[i]);
+    //     }
+    //    return true; 
+
+    int[] dp=new int[nums.length];
+    Arrays.fill(dp,nums.length);
+    return mj(nums,dp);
+    }
+    public static boolean mj(int[] nums,int[] dp)
+    {
+        if(nums.length==1 && nums[0]==0)
+        return true;
+        if(nums[0]==0)
         return false;
-        m=Math.max(m,i+nums[i]);
-     }
-     return true;
+        dp[0]=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=1;j<=nums[i];j++)
+            {
+                if(i+j<nums.length)
+                {
+                    dp[i+j]=Math.min(dp[i+j],dp[i]+1);
+                }
+            }
+        }
+        if(dp[nums.length-1]==nums.length)  return false;
+
+        return true;
+
     }
 }
