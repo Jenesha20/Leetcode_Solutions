1ef40639efa858d8dd05fdfb20d4a19a511d8c46
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2025-06-24 11:06:02 +0530

    Time: 7 ms (64.37%) | Memory: 44.9 MB (35.72%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,33 +8,47 @@
  * }
  */
 class Solution {
-    List<TreeNode> l1=new ArrayList<>();
-    List<TreeNode> l2=new ArrayList<>();
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        find(root,p,l1);
-        find(root,q,l2);
-        TreeNode res=null;
-        for(int i=l1.size()-1;i>=0;i--)
-        {
-            if(l2.contains(l1.get(i)))
-            {
-                res=l1.get(i);
-                break;
-            }
-        }
-        return res;
-    }
-    public boolean find(TreeNode root,TreeNode target,List<TreeNode> l)
-    {
-        if(root==null)
-        return false;
-        l.add(root);
-        if(root==target)
-        return true;
-        if(find(root.left,target,l) || find(root.right,target,l))
-        return true;
+    // List<TreeNode> l1=new ArrayList<>();
+    // List<TreeNode> l2=new ArrayList<>();
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     find(root,p,l1);
+    //     find(root,q,l2);
+    //     TreeNode res=null;
+    //     for(int i=l1.size()-1;i>=0;i--)
+    //     {
+    //         if(l2.contains(l1.get(i)))
+    //         {
+    //             res=l1.get(i);
+    //             break;
+    //         }
+    //     }
+    //     return res;
+    // }
+    // public boolean find(TreeNode root,TreeNode target,List<TreeNode> l)
+    // {
+    //     if(root==null)
+    //     return false;
+    //     l.add(root);
+    //     if(root==target)
+    //     return true;
+    //     if(find(root.left,target,l) || find(root.right,target,l))
+    //     return true;
 
-        l.remove(l.size()-1);
-        return false;
+    //     l.remove(l.size()-1);
+    //     return false;
+    // }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return left != null ? left : right;    
     }
 }
