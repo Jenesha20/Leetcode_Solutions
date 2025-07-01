328679a8f83175dd52cab57c0af67a082b201957
Jenesha <166708484+Jenesha20@users.noreply.github.com>
2025-07-01 10:14:21 +0530

    Time: 10 ms (83.69%) | Memory: 49.6 MB (5.80%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,34 +1,53 @@
 class Solution {
-    public int networkDelayTime(int[][] times, int n, int k) {
-        int[] dis=new int[n+1];
-        int e=times.length;
-        for(int i=1;i<=n;i++)
+    class Pair
+    {
+        int node;
+        int dis;
+        Pair(int node,int dis)
         {
-            dis[i]=(int)1e9;
+            this.node=node;
+            this.dis=dis;
         }
-        dis[k]=0;
-        for(int i=0;i<n-1;i++)
+    }
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<int[]>> adj=new ArrayList<>();
+        for(int i=1;i<=n+1;i++)
+        adj.add(new ArrayList<>());
+        for(int[] x:times)
         {
-            for(int j=0;j<e;j++)
+           int u=x[0];
+           int v=x[1];
+           int w=x[2];
+           adj.get(u).add(new int[]{v,w});
+        }
+        int[] dist=new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k]=0;
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b) -> a.dis-b.dis);
+        pq.add(new Pair(k,0));
+        while(!pq.isEmpty())
+        {
+            Pair cur=pq.poll();
+            int node=cur.node;
+            int d=cur.dis;
+            for(int[] neigh:adj.get(node))
             {
-                int u=times[j][0];
-                int v=times[j][1];
-                int wt=times[j][2];
-                if(dis[u]+wt<dis[v])
+                int u=neigh[0];
+                int wt=neigh[1];
+                if(d+wt<dist[u])
                 {
-                    dis[v]=dis[u]+wt;
+                    dist[u]=d+wt;
+                    pq.add(new Pair(u,dist[u]));
                 }
             }
         }
-        int m=0;
-        for(int x:dis)
+        int max=0;
+        for(int i=1;i<n+1;i++)
         {
-            if(x==(int)1e9)
-            {
-                return -1;
-            }
-            m=Math.max(m,x);
+            if(dist[i]==Integer.MAX_VALUE)
+            return -1;
+            max=Math.max(max,dist[i]);
         }
-        return m;
+        return max;
     }
 }
